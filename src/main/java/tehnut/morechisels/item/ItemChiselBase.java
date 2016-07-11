ae9d4d099a02310a00d8bd73cfa1a73c5b8b4c08
RCXcrafter <rcxcrafter@gmail.com>
2016-07-11 22:39:33 +0200

    all chisels now have proper tooltips and attack damage

--- src/main/java/tehnut/morechisels/item/ItemChiselBase.java
+++ src/main/java/tehnut/morechisels/item/ItemChiselBase.java
@@ -1,9 +1,18 @@
 package tehnut.morechisels.item;
 
+import java.util.List;
+
 import com.cricketcraft.chisel.api.ChiselTabs;
 import com.cricketcraft.chisel.api.IChiselItem;
 import com.cricketcraft.chisel.api.carving.ICarvingVariation;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+
 import net.minecraft.block.Block;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -14,6 +23,8 @@ public class ItemChiselBase extends Item implements IChiselItem {
 
     private ChiselType type;
     protected boolean hasModes = false;
+    protected boolean canChiselBlock = true;
+    protected int attackDamage = 2;
 
     /**
      *
@@ -48,12 +59,26 @@ public class ItemChiselBase extends Item implements IChiselItem {
     }
 
     /**
-     * @return - Returns weather the chisel should be rendered like a tool in third persson.
+     * @return - Returns whether the chisel should be rendered like a tool in third persson.
      */
 	public boolean isFull3D() {
 		return true;
 	}
 
+	@SuppressWarnings("rawtypes")
+	@Override
+	public Multimap getAttributeModifiers(ItemStack stack) {
+		Multimap<String, AttributeModifier> multimap = HashMultimap.create();
+		multimap.put(SharedMonsterAttributes.attackDamage.getAttributeUnlocalizedName(), new AttributeModifier(field_111210_e, "Chisel Damage", attackDamage, 0));
+		return multimap;
+	}
+
+	@Override
+	public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker) {
+		stack.damageItem(1, attacker);
+		return super.hitEntity(stack, attacker, target);
+}
+	
     // Start Chisel API
 
     @Override
@@ -73,11 +98,30 @@ public class ItemChiselBase extends Item implements IChiselItem {
 
     @Override
     public boolean canChiselBlock(World world, EntityPlayer player, int x, int y, int z, Block block, int metadata) {
-        return true;
+        return canChiselBlock;
     }
 
     @Override
     public boolean hasModes(ItemStack chisel) {
         return hasModes;
     }
+    
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean held) {
+		String base = "item.chisel.chisel.desc.";
+		String gui = I18n.format(base + "gui");
+		String lc1 = I18n.format(base + "lc1");
+		String lc2 = I18n.format(base + "lc2");
+		String modes = I18n.format(base + "modes");
+		list.add(gui);
+		if (canChiselBlock) {
+			list.add(lc1);
+			list.add(lc2);
+		}
+		if (hasModes) {
+			list.add("");
+			list.add(modes);
+		}
+	}
 }
