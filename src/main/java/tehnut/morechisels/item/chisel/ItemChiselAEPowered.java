f305c9aa8006aa2078362d100966b4a9fdad47ed
RCXcrafter <rcxcrafter@gmail.com>
2016-07-13 17:57:38 +0200

    Temporarily give up on AE powered chisel

--- src/main/java/tehnut/morechisels/item/chisel/ItemChiselAEPowered.java
+++ src/main/java/tehnut/morechisels/item/chisel/ItemChiselAEPowered.java
@@ -10,45 +10,15 @@ import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 import tehnut.morechisels.item.ChiselType;
 import tehnut.morechisels.item.ItemChiselBase;
-import appeng.api.config.AccessRestriction;
-import appeng.api.implementations.items.IAEItemPowerStorage;
+//import appeng.api.config.AccessRestriction;
+//import appeng.api.implementations.items.IAEItemPowerStorage;
 
 import java.util.List;
 
-public class ItemChiselAEPowered extends ItemChiselBase implements IAEItemPowerStorage {
+public class ItemChiselAEPowered extends ItemChiselBase { //implements IAEItemPowerStorage {
 
     public ItemChiselAEPowered() {
         super(ChiselType.AE);
     }
-
-	@Override
-	public double injectAEPower(ItemStack is, double amt) {
-		// TODO Auto-generated method stub
-		return 1;
-	}
-
-	@Override
-	public double extractAEPower(ItemStack is, double amt) {
-		// TODO Auto-generated method stub
-		return 1;
-	}
-
-	@Override
-	public double getAEMaxPower(ItemStack is) {
-		// TODO Auto-generated method stub
-		return 1;
-	}
-
-	@Override
-	public double getAECurrentPower(ItemStack is) {
-		// TODO Auto-generated method stub
-		return 1;
-	}
-
-	@Override
-	public AccessRestriction getPowerFlow(ItemStack is) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 	//I have no idea what I'm doing
 }
